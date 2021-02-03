35ab65a61eb58f79b719fa689dd2f1c74d999049
Lhumd <elham.daneshmand@yahoo.com>
2021-02-03 14:59:30 +0100

    Reformat code

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-        packages=['bolt'],
-        package_dir={'': 'python'}
-)
+d = generate_distutils_setup(packages=["bolt"], package_dir={"": "python"})
 
 setup(**d)
