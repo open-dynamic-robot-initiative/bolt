79bb5e560071e4382299035b7afaa0b0280af1bb
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-03-03 17:53:54 +0100

    fix previous commit

---
+++ demos/bolt_demo_calibration.cpp
@@ -0,0 +1,74 @@
+/**
+ * @file demo_solo12_calibration.cpp
+ * @author Maximilien Naveau (maximilien.naveau@gmail.com)
+ * @brief Small demo to test the calibration on the real robot.
+ * @version 0.1
+ * @date 2019-11-08
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#include "bolt/bolt.hpp"
+#include "bolt/utils.hpp"
+
+using namespace bolt;
+typedef ThreadCalibrationData<Bolt> ThreadCalibrationData_t;
+
+static THREAD_FUNCTION_RETURN_TYPE control_loop(void* thread_data_void_ptr)
+{
+    ThreadCalibrationData_t* thread_data_ptr =
+        (static_cast<ThreadCalibrationData_t*>(thread_data_void_ptr));
+
+    Bolt& robot = *thread_data_ptr->robot;
+
+    // ask for calibration
+    robot.request_calibration();
+
+    // The calibration commend is computed in the send_target_joint_torque.
+    Eigen::Vector6d dummy_command = Eigen::Vector6d::Zero();
+    real_time_tools::Spinner spinner;
+    spinner.set_period(0.001);
+    while (!CTRL_C_DETECTED && robot.is_calibrating())
+    {
+        robot.acquire_sensors();
+        robot.send_target_joint_torque(dummy_command);
+        spinner.spin();
+    }
+
+    CTRL_C_DETECTED = true;
+    return THREAD_FUNCTION_RETURN_VALUE;
+}  // end control_loop
+
+int main(int argc, char** argv)
+{
+    enable_ctrl_c();
+
+    if (argc != 2)
+    {
+        throw std::runtime_error(
+            "Wrong number of argument: `./demo_bolt_calibration "
+            "network_id`.");
+    }
+
+    std::shared_ptr<Bolt> robot = std::make_shared<Bolt>();
+    robot->initialize(argv[1]);
+
+    ThreadCalibrationData_t thread_data;
+    thread_data.robot = robot;
+
+    rt_printf("Controller is set up.\n");
+    rt_printf("Press enter to launch the calibration.\n");
+    char str[256];
+    std::cin.get(str, 256);  // get c-string
+
+    real_time_tools::RealTimeThread thread;
+    thread.create_realtime_thread(&control_loop, &thread_data);
+
+    // Wait until the application is killed.
+    thread.join();
+
+    rt_printf("Exit cleanly.\n");
+
+    return 0;
+}
---
+++ demos/bolt_demo_pd.cpp
@@ -0,0 +1,109 @@
+/**
+ * \file demo_bolt.cpp
+ * \brief Implements basic PD controller reading slider values.
+ * \author Julian Viereck
+ * \date 21 November 2019
+ *
+ * This file uses the Solo12 class in a small demo.
+ */
+
+#include "bolt/bolt.hpp"
+#include "bolt/utils.hpp"
+
+using namespace bolt;
+
+static THREAD_FUNCTION_RETURN_TYPE control_loop(void* args)
+{
+    Bolt& robot = *static_cast<Bolt*>(args);
+
+    // Using conversion from PD gains from example.cpp
+    double kp = 3.0 * 9 * 0.025;
+    double kd = 0.1 * 9 * 0.025;
+    double t = 0.0;
+    double dt = 0.001;
+    double freq = 0.3;
+    double amplitude = M_PI / 8.0;
+    Eigen::Vector6d desired_joint_position = Eigen::Vector6d::Zero();
+    Eigen::Vector6d desired_torque = Eigen::Vector6d::Zero();
+
+    Eigen::Vector6d init_pose;
+    Eigen::Matrix<bool, 6, 1> motor_enabled;
+
+    robot.acquire_sensors();
+    Eigen::Vector6d initial_joint_positions = robot.get_joint_positions();
+
+    rt_printf("control loop started \n");
+
+    size_t count = 0;
+    while (!CTRL_C_DETECTED)
+    {
+        // acquire the sensors
+        robot.acquire_sensors();
+
+        // acquire the motor enabled signal.
+        motor_enabled = robot.get_motor_enabled();
+
+        // Desired pose and vel
+        desired_joint_position =
+            initial_joint_positions +
+            Eigen::Vector6d::Ones() * amplitude * sin(2 * M_PI * freq * t);
+        t += dt;
+
+        // we implement here a small pd control at the current level
+        desired_torque =
+            kp * (desired_joint_position - robot.get_joint_positions()) -
+            kd * robot.get_joint_velocities();
+
+        // print -----------------------------------------------------------
+        if ((count % 1000) == 0)
+        {
+            rt_printf("\33[H\33[2J");  // clear screen
+            print_vector("des joint_tau  : ", desired_torque);
+            print_vector("des joint_pos  : ", desired_joint_position);
+            rt_printf("\n");
+            print_vector("act joint_pos  : ", robot.get_joint_positions());
+            print_vector("act joint_vel  : ", robot.get_joint_velocities());
+            print_vector("act slider pos : ", robot.get_slider_positions());
+            rt_printf("act e-stop     : %s\n",
+                      robot.get_active_estop() ? "true" : "false");
+
+            fflush(stdout);
+        }
+        ++count;
+
+        // Send the current to the motor
+        // desired_torque.setZero();
+        robot.send_target_joint_torque(desired_torque);
+
+        real_time_tools::Timer::sleep_sec(0.001);
+    }  // endwhile
+    return THREAD_FUNCTION_RETURN_VALUE;
+}  // end control_loop
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        throw std::runtime_error(
+            "Wrong number of argument: `./demo_bolt network_id`.");
+    }
+
+    real_time_tools::RealTimeThread thread;
+    enable_ctrl_c();
+
+    Bolt robot;
+    robot.initialize(std::string(argv[1]));
+
+    rt_printf("controller is set up \n");
+    thread.create_realtime_thread(&control_loop, &robot);
+
+    rt_printf("control loop started \n");
+    while (!CTRL_C_DETECTED)
+    {
+        real_time_tools::Timer::sleep_sec(0.001);
+    }
+
+    thread.join();
+
+    return 0;
+}
