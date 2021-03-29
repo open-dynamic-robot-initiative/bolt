72bed09c2051cdd87155d6786dca0cced0784875
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2021-03-29 16:58:30 +0200

    work on simulating the passive ankle

--- python/bolt/dg_bolt_bullet.py
+++ python/bolt/dg_bolt_bullet.py
@@ -1,9 +1,16 @@
-from __future__ import print_function
+"""
+
+License: BSD 3-Clause License
+Copyright (C) 2018-2021, New York University , Max Planck Gesellschaft
+Copyright note valid unless otherwise stated in individual files.
+All rights reserved.
+"""
+
 from robot_properties_bolt.config import BoltConfig
-from dg_bolt.bolt_base_bullet import BoltBaseRobot
+from bolt.dg_bolt_base_bullet import DgBoltBaseRobot
 
 
-class BoltBulletRobot(BoltBaseRobot):
+class BoltBulletRobot(DgBoltBaseRobot):
     def __init__(
         self,
         useFixedBase=False,
@@ -25,7 +32,7 @@ class BoltBulletRobot(BoltBaseRobot):
         self.q0[12] = -1.57079633
 
         # Sync the current robot state to the graph input signals.
-        self.sim2signal_()
+        self._sim2signal()
 
 
 def get_bolt_robot(
