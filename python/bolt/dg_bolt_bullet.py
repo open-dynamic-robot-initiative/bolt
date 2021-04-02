7b5c5f181d6146ac056af1a158182ce808377d42
Maximilien Naveau <maximilien.naveau@gmail.com>
2021-04-02 18:10:05 +0200

    fix dg_simu

--- python/bolt/dg_bolt_bullet.py
+++ python/bolt/dg_bolt_bullet.py
@@ -13,12 +13,13 @@ from bolt.dg_bolt_base_bullet import DgBoltBaseRobot
 class BoltBulletRobot(DgBoltBaseRobot):
     def __init__(
         self,
-        useFixedBase=False,
-        record_video=False,
+        use_fixed_base=False,
         init_sliders_pose=4 * [0.5],
     ):
         super(BoltBulletRobot, self).__init__(
-            BoltConfig(), useFixedBase, record_video, init_sliders_pose
+            BoltConfig(),
+            use_fixed_base=use_fixed_base,
+            init_sliders_pose=init_sliders_pose,
         )
 
         self.q0[2] = 0.26487417
@@ -35,7 +36,5 @@ class BoltBulletRobot(DgBoltBaseRobot):
         self._sim2signal()
 
 
-def get_bolt_robot(
-    useFixedBase=False, record_video=False, init_sliders_pose=4 * [0.5]
-):
-    return BoltBulletRobot(useFixedBase, record_video, init_sliders_pose)
+def get_bolt_robot(use_fixed_base=False, init_sliders_pose=4 * [0.5]):
+    return BoltBulletRobot(use_fixed_base, init_sliders_pose)
